257eefaa68326d4598b9bde759b4755783b7bc26
Seungjae Park <astro.psj@gmail.com>
2018-05-14 23:36:04 +0900

    Add sentence spliter function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 extern crate hangeul;
+extern crate regex;
+
+use regex::Regex;
 
 fn compose_all(components: &Vec<(char, char, char)>) -> String {
     components
@@ -24,6 +27,29 @@ fn decompose_all(content: &str) -> Vec<(char, char, char)> {
         .collect()
 }
 
+fn split(content: &str) -> Vec<String> {
+    let alphabet = Regex::new("^[A-z]+$").unwrap();
+    let hangul = Regex::new("^[가-힣]+$").unwrap();
+    let number = Regex::new("^[0-9]+$").unwrap();
+    let mut chunks = Vec::<String>::new();
+    let mut previous_chunk = "".to_string();
+    for s in content.chars().map(|c| c.to_string()) {
+        if (alphabet.is_match(&previous_chunk) && alphabet.is_match(&s))
+            || (hangul.is_match(&previous_chunk) && hangul.is_match(&s))
+            || (number.is_match(&previous_chunk) && number.is_match(&s))
+            || &previous_chunk == ""
+        {
+            previous_chunk.push_str(&s);
+        } else {
+            chunks.push(previous_chunk);
+            previous_chunk = "".to_string();
+            previous_chunk.push_str(&s);
+        }
+    }
+    chunks.push(previous_chunk);
+    chunks
+}
+
 pub struct Kokoa {}
 
 impl Kokoa {
@@ -31,7 +57,8 @@ impl Kokoa {
         Kokoa {}
     }
 
-    pub fn train(&self) -> bool {
+    pub fn train(&self, document: &str) -> bool {
+        println!("{}", &document);
         true
     }
 
@@ -47,4 +74,5 @@ fn main() {
     let sentence = "안녕하세요!";
     let kokoa = Kokoa::new();
     kokoa.run(&sentence);
+    println!("{:?}", split("안녕ㅎㅎㅎㅎ12 4rr ?"))
 }
