8e69ef970bbfc2b301853b53daf4b652adf7ab37
Seungjae Park <astro.psj@gmail.com>
2018-05-14 22:13:08 +0900

    Refactor the code and add composer function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 extern crate hangeul;
 
-fn decompose(s: &str) -> Vec<(char, char, char)> {
-    let mut components = Vec::new();
-    for c in s.chars() {
-        components.push((
-            hangeul::get_choseong(c).unwrap_or(c),
-            hangeul::get_jungseong(c).unwrap_or_default(),
-            hangeul::get_jongseong(c)
-                .unwrap_or_default()
-                .unwrap_or_default(),
-        ));
-    }
+fn compose_all(components: &Vec<(char, char, char)>) -> String {
     components
+        .into_iter()
+        .map(|component| {
+            hangeul::compose(component.0, component.1, Some(component.2)).unwrap_or(component.0)
+        })
+        .collect()
+}
+
+fn decompose_all(content: &str) -> Vec<(char, char, char)> {
+    content
+        .chars()
+        .map(|c| {
+            (
+                hangeul::get_choseong(c).unwrap_or(c),
+                hangeul::get_jungseong(c).unwrap_or_default(),
+                hangeul::get_jongseong(c)
+                    .unwrap_or_default()
+                    .unwrap_or_default(),
+            )
+        })
+        .collect()
 }
 
 pub struct Kokoa {}
@@ -27,7 +37,9 @@ impl Kokoa {
 
     pub fn run(&self, document: &str) {
         println!("{}", &document);
-        println!("{:?}", decompose(&document))
+        let d = decompose_all(&document);
+        println!("{:?}", &d);
+        println!("{}", compose_all(&d));
     }
 }
 
