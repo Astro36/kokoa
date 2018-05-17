e1b8cd99e8f7e9eb7919ed844280e897d4d62345
Seungjae Park <astro.psj@gmail.com>
2018-05-17 18:58:31 +0900

    Update the code

--- bin/train-namuwiki.js
+++ bin/train-namuwiki.js
@@ -15,11 +15,18 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 const fs = require('fs');
+const StreamArray = require('stream-json/utils/StreamArray');
 
 const Kokoa = require('../lib');
 
 const kokoa = new Kokoa();
-kokoa.train(fs.readFileSync('./data/the-battle-of-midway.txt').toString());
-kokoa.update();
-fs.writeFileSync('./data/namuwiki-words.json', JSON.stringify(kokoa.save()));
-console.log(kokoa.run('태평양 전쟁 개전 당시 일본 해군의 전략은 외곽 방어망 곳곳에 배치된 지상비행장이 방어의 근거지가 되고 유사시 적을 방어선 가까이 끌어들이는 동안 항공기를 집결하여 격퇴하는 것이었다'));
+
+const stream = StreamArray.make();
+stream.output.on('data', object => kokoa.train(object.value.text));
+stream.output.on('end', () => {
+  console.log('done');
+  kokoa.update();
+  fs.writeFileSync('./data/namuwiki-words.json', JSON.stringify(kokoa.save()));
+});
+
+fs.createReadStream('./data/namuwiki.json').pipe(stream.input);
