77f30cf9872844c2f1a998acaad92eaf64b1b080
Seungjae Park <astro.psj@gmail.com>
2018-05-23 00:25:17 +0900

    Update util code

--- test/index.js
+++ test/index.js
@@ -16,10 +16,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 const fs = require('fs');
 
-const Kokoa = require('../lib');
+const { KokoaUtil } = require('../lib');
 
-const kokoa = new Kokoa();
-kokoa.train(fs.readFileSync('./data/the-battle-of-midway.txt').toString());
-kokoa.update();
-// fs.writeFileSync('./data/namuwiki-words.json', JSON.stringify(kokoa.save()));
-console.log(kokoa.run('태평양 전쟁 개전 당시 일본 해군의 전략은 외곽 방어망 곳곳에 배치된 지상비행장이 방어의 근거지가 되고 유사시 적을 방어선 가까이 끌어들이는 동안 항공기를 집결하여 격퇴하는 것이었다'));
+const content = fs.readFileSync('./data/the-battle-of-midway.txt').toString();
+
+const util = new KokoaUtil();
+console.log(util.keysentences(content));
+// console.log(util.words('태평양 전쟁 개전 당시 일본 해군의 전략은 외곽 방어망 곳곳에 배치된 지상비행장이 방어의 근거지가 되고 유사시 적을 방어선 가까이 끌어들이는 동안 항공기를 집결하여 격퇴하는 것이었다'));
