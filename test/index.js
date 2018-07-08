c2ea584d193f9507e5b705b8349739abb4ae7ed1
Seungjae Park <astro.psj@gmail.com>
2018-07-08 20:23:58 +0900

    Add spacing engine and update docs

--- test/index.js
+++ test/index.js
@@ -15,7 +15,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 const { Kokoa, model: { SpacingEngine, WordsEngine } } = require('../lib');
+const fs = require('fs');
 
-const wordsEngine = WordsEngine.load('./data/words.csv');
-const kokoa = new Kokoa({wordsEngine});
-console.log(kokoa.words('태평양 전쟁 개전 당시 일본 해군의 전략은 외곽 방어망 곳곳에 배치된 지상비행장이 방어의 근거지가 되고 유사시 적을 방어선 가까이 끌어들이는 동안 항공기를 집결하여 격퇴하는 것이었다.'));
+const kokoa = new Kokoa();// Kokoa.load('./data/kokoa.*.csv');
+
+kokoa.train(fs.readFileSync('./data/test.txt').toString());
+kokoa.update();
+console.log(kokoa.spacing('람다대수는알론조처치에의해튜링기계보다먼저제안된계산모델이며람다대수로할수있는모든계산은튜링기계로도가능하고그역도성립한다는것이증명되었다.람다대수와동치인언어역시튜링완전하다.프로그래밍언어에서는튜링기계보다람다대수를더널리이용한다.'));
+console.log(kokoa.words('람다 대수는 알론조 처치에 의해 튜링 기계보다 먼저 제안된 계산 모델이며 람다 대수로 할 수 있는 모든 계산은 튜링 기계로도 가능하고 그 역도 성립한다는 것이 증명되었다. 람다 대수와 동치인 언어 역시 튜링 완전하다. 프로그래밍 언어에서는 튜링 기계보다 람다 대수를 더 널리 이용한다.'));
